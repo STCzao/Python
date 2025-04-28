e49e75c94ee5bfc6603742f04113ddc76ed13b64
STCzao <Santicoronel0183@gmail.com>
2025-04-28 14:26:21 -0300

    Se agregaron ejercicios de entrada de datos

---
+++ Entradadedatos1.py
@@ -0,0 +1,14 @@
+#PRIMERA ENTRADA DE DATOS
+
+palabra = input("Introduce una palabra: ")
+num_int = int(input("Introduce un numero entero: "))
+num_float = float(input("Introduce un numero flotante: "))
+num_compless = complex(input("Introduce un numero complejo: "))
+
+print("String: ", palabra)
+print("Entero: ", num_int)
+print("Flotante: ", num_float)
+print("Complejo: ", num_compless)
+
+
+
